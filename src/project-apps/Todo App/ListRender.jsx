321e28463e7216a0432da354eb752b582649f9a3
praneeth <gp.gunturu@gmail.com>
2025-04-15 20:49:36 -0500

    rendering the list of dummy tasks via map

--- src/project-apps/Todo App/ListRender.jsx
+++ src/project-apps/Todo App/ListRender.jsx
@@ -2,6 +2,12 @@ import DUMMY_TASKS from './tasks.js'
 
 export default function ListRender() {
     return (
-        <p>{DUMMY_TASKS[1]}</p>
+        <div>
+            <ul>
+                {DUMMY_TASKS.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
