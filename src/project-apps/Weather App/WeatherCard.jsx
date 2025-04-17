38e466d45f04d4ee2e5a2069278836e687686ddf
praneeth <gp.gunturu@gmail.com>
2025-04-17 17:28:14 -0500

    Dynamically displaying the city name and weather details

--- src/project-apps/Weather App/WeatherCard.jsx
+++ src/project-apps/Weather App/WeatherCard.jsx
@@ -1,8 +1,19 @@
 export default function WeatherCard({weatherData}) {
+
+    const cityName = weatherData?.city?.name
+    const temp = weatherData?.list[0]?.main?.temp
+    const description = weatherData?.list[0]?.weather[0]?.description
+    const icon = weatherData?.list[0]?.weather[0]?.icon
+    const dt_txt = weatherData?.list[0]?.dt_txt
+    
+    const fahrenheitTemperature = ((temp-273.15)*9/5)+32
+
     return (
         <div>
-            <h1>Todays weather at {weatherData?.city?.name}</h1>
-            <p>37 degrees</p>
+            <h1>{cityName ? `Todays weather at ${cityName}` : null}</h1>
+            <p>{description}</p>
+            <p>{fahrenheitTemperature ? `temperature: ${fahrenheitTemperature} degrees fahrenheit` : null}</p>
+            {icon ? <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} /> : null}
         </div>
     )
 }
