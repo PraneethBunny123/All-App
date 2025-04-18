dae3982b3a2a6d25cf7e3e1680059e0db68573ef
praneeth <gp.gunturu@gmail.com>
2025-04-18 09:51:46 -0500

    enabling search via pressing enter key

--- src/project-apps/Weather App/SearchBar.jsx
+++ src/project-apps/Weather App/SearchBar.jsx
@@ -1,10 +1,15 @@
 export default function SearchBar({ handleChange, handleFetch, city }) {
+    function handleEnterKey(e) {
+        if(e.key === 'Enter') handleFetch()
+    }
+
     return (
         <div className="flex gap-2 mb-6 items-center justify-center">
             <input 
                 placeholder="Enter city"
                 type="text"
                 onChange={handleChange}
+                onKeyDown={handleEnterKey}
                 value={city}
                 className="px-4 py-2 rounded-lg bg-white text-gray-800 shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 w-64"
             />
