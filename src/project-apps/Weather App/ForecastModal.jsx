00ba0c550bf400bd8e1e1c3529aab220d6aa9cff
praneeth <gp.gunturu@gmail.com>
2025-04-18 17:05:35 -0500

    added a button on modal to close on click

--- src/project-apps/Weather App/ForecastModal.jsx
+++ src/project-apps/Weather App/ForecastModal.jsx
@@ -2,9 +2,18 @@ import { createPortal } from "react-dom"
 import ForecastCard from "./ForecastCard"
 
 const ForecastModal = ({data, dialogRef}) => {
+    function handleModalClose() {
+        dialogRef.current.close()
+    }
 
     return createPortal(
         <dialog ref={dialogRef}>
+            <button 
+                onClick={handleModalClose}
+                
+            >
+                ❌
+            </button>
             <ForecastCard data={data} variant="modal"/>
         </dialog>,
         document.getElementById('modal')
