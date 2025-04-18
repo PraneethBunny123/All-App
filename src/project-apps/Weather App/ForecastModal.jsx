0f9889b603b7c3a48aa38cfb750695e7e33916ff
praneeth <gp.gunturu@gmail.com>
2025-04-18 17:17:03 -0500

    added few styles to the modal

--- src/project-apps/Weather App/ForecastModal.jsx
+++ src/project-apps/Weather App/ForecastModal.jsx
@@ -8,10 +8,13 @@ const ForecastModal = ({data, dialogRef, onClose}) => {
     }
 
     return createPortal(
-        <dialog ref={dialogRef}>
+        <dialog 
+            ref={dialogRef} 
+            className="rounded-lg p-6 bg-white w-[90%] max-w-md shadow-xl relative"    
+        >
             <button 
                 onClick={handleModalClose}
-                
+                className="absolute top-2 right-2 text-gray-500 hover:text-black"
             >
                 ❌
             </button>
