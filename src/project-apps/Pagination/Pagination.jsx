0ef4acb5d7ca27637bb5d1e019fca9c386ae9749
praneeth <gp.gunturu@gmail.com>
2025-04-20 14:09:55 -0500

    Added slice method to render items per page. added buttons to increase and decrease the currentPage value. Finally completed the basic logic of the pagiation

--- src/project-apps/Pagination/Pagination.jsx
+++ src/project-apps/Pagination/Pagination.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
 import DATA from "./data"
 
 export default function Pagination() {
+    const itemsPerPage = 5
+
+    const [currentPage, setCurrentPage] = useState(1)
+    console.log(currentPage)
+
+    const totalNumberOfPages = Math.ceil(DATA.length / itemsPerPage)
+
+    const startIndex = (currentPage-1)*itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+
     return (
         <div>
             <ul>
-                {DATA.map(data => (
+                {DATA.slice(startIndex, endIndex).map(data => (
                     <li key={data}>{data}</li>
                 ))}
             </ul>
+            <div>
+                <button disabled={currentPage===1} onClick={() => setCurrentPage(prev => prev-1)}>prev</button>
+                <button>{currentPage}</button>
+                <button disabled={currentPage===totalNumberOfPages} onClick={() => setCurrentPage(prev => prev+1)}>next</button>
+            </div>
         </div>
     )
 }
