345511ad91fd8b99690234429d3e8ada17b4f266
praneeth <gp.gunturu@gmail.com>
2025-04-19 21:59:59 -0500

    Added logic to get data to state when button is clicked

--- src/project-apps/Accordion/RenderTab.jsx
+++ src/project-apps/Accordion/RenderTab.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 import TABS from "./tabData"
 
 export default function RenderTab() {
+    const [currentTabData, setCurrentTabData] = useState(null)
+
+    function handleClick(data) {
+        setCurrentTabData(data)
+    }
+
+    console.log(currentTabData)
+
     return (
         <div>
             <ul>
                 {TABS.map(data => (
                     <li key={data.label}>
-                        <button>{data.label}</button>
+                        <button onClick={() => handleClick(data)}>{data.label}</button>
                     </li>
                 ))}
             </ul>
