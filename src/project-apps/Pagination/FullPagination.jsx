cece8a4066a9bcd679b872b76036e665bbccc156
praneeth <gp.gunturu@gmail.com>
2025-04-20 20:11:24 -0500

    Added some failed logic to display some pages

--- src/project-apps/Pagination/FullPagination.jsx
+++ src/project-apps/Pagination/FullPagination.jsx
@@ -18,20 +18,40 @@ export default function FullPagination() {
         setCurrentPage(prev => prev+1)
     }
 
+    function handleButtonOne() {
+        setCurrentPage(1)
+    }
+
+    function handleButtonTwo() {
+        setCurrentPage(2)
+    }
+
+    function handleButtonThree() {
+        setCurrentPage(3)
+    }
+
+    function handleButtonFour() {
+        setCurrentPage(4)
+    }    
+
+    function handleButtonFive() {
+        setCurrentPage(5)
+    }
 
     return (
         <div>
+            <h1>Page: {currentPage}</h1>
             <ul>
                 {LARGE_DATA.slice(startIndex, endIndex).map(data => (
                     <li key={data.id}>{data.name}</li>
                 ))}
             </ul>
             <button disabled={currentPage===1} onClick={handlePrevButton}>prev</button>
-            <button>{currentPage}</button>
-            <button>{currentPage+1}</button>
-            <button>{currentPage+2}</button>
-            <button>{currentPage+3}</button>
-            <button>{currentPage+4}</button>
+            <button onClick={handleButtonOne}>{currentPage}</button>
+            <button onClick={handleButtonTwo}>{currentPage+1}</button>
+            <button onClick={handleButtonThree}>{currentPage+2}</button>
+            <button onClick={handleButtonFour}>{currentPage+3}</button>
+            <button onClick={handleButtonFive}>{currentPage+4}</button>
             <button disabled={currentPage===totalPages} onClick={handleNextButton}>next</button>
         </div>
     )
