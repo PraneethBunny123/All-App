eef5cae3e1dddf08d9622bdae7251144c41ac9e3
praneeth <gp.gunturu@gmail.com>
2025-04-20 20:23:50 -0500

    Displaying page numbers dynamically using Array.from method to get page numbers

--- src/project-apps/Pagination/FullPagination.jsx
+++ src/project-apps/Pagination/FullPagination.jsx
@@ -5,10 +5,14 @@ export default function FullPagination() {
     const [currentPage, setCurrentPage] = useState(1)
 
     const itemsPerPage = 5
+    const pageNumbersPerPage = 5
     const totalPages = Math.ceil(LARGE_DATA.length / itemsPerPage)
 
     const startIndex = (currentPage-1) * itemsPerPage
     const endIndex = startIndex + itemsPerPage
+
+    const pages = Array.from({length: totalPages}, (_, i) => i+1)
+    console.log(pages)
 
     function handlePrevButton(){
         setCurrentPage(prev => prev-1)
@@ -47,11 +51,7 @@ export default function FullPagination() {
                 ))}
             </ul>
             <button disabled={currentPage===1} onClick={handlePrevButton}>prev</button>
-            <button onClick={handleButtonOne}>{currentPage}</button>
-            <button onClick={handleButtonTwo}>{currentPage+1}</button>
-            <button onClick={handleButtonThree}>{currentPage+2}</button>
-            <button onClick={handleButtonFour}>{currentPage+3}</button>
-            <button onClick={handleButtonFive}>{currentPage+4}</button>
+            {pages.map(page => <button key={page}>{page}</button>)}
             <button disabled={currentPage===totalPages} onClick={handleNextButton}>next</button>
         </div>
     )
