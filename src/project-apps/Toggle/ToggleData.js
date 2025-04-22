768e41f4367d7ec49f40f4370e85bc7c89b25677
praneeth <gp.gunturu@gmail.com>
2025-04-21 23:08:45 -0500

    Created a dummy data and using that data to render the items

---
+++ src/project-apps/Toggle/ToggleData.js
@@ -0,0 +1,6 @@
+const TOGGLE_DATA = Array.from({length: 15}, (_, i) => ({
+    id: i+1,
+    item: `item ${i+1}`
+}))
+
+export default TOGGLE_DATA
--- src/project-apps/Toggle/ToggleDetails.jsx
+++ src/project-apps/Toggle/ToggleDetails.jsx
@@ -1,7 +1,13 @@
+import TOGGLE_DATA from './ToggleData'
+
 export default function ToggleDetails() {
     return (
         <div>
-            Hello
+        <ul>
+            {TOGGLE_DATA.map(data => (
+                <li key={data.item}>{data.item}</li>
+            ))}
+        </ul>
         </div>
     )
 }
