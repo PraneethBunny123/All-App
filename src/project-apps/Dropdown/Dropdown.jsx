479fc16ccb3cf8c8fd512664ec6a2e9a7aa47f42
praneeth <gp.gunturu@gmail.com>
2025-04-22 17:51:26 -0500

    added state and set that to selected country and  console logging it

--- src/project-apps/Dropdown/Dropdown.jsx
+++ src/project-apps/Dropdown/Dropdown.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import COUNTRIES from "./countries"
 
 export default function Dropdown() {
+    const [selectedCountry, setSelectedCountry] = useState('')
+
+    function handleOnChange(e) {
+        setSelectedCountry(e.target.value)
+    }
+
+    console.log(selectedCountry)
+
     return (
         <div>
-            <select>
+            <select value={selectedCountry} onChange={handleOnChange}>
                 {COUNTRIES.map(country => (
                     <option key={country}>{country}</option>
                 ))}  
