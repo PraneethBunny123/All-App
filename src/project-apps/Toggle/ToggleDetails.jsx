58885ebc33212cf729305a901cc07f58562a0b9d
praneeth <gp.gunturu@gmail.com>
2025-04-21 23:14:23 -0500

    Setup state and using button click to set the state value to current id

--- src/project-apps/Toggle/ToggleDetails.jsx
+++ src/project-apps/Toggle/ToggleDetails.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import TOGGLE_DATA from './ToggleData'
 
 export default function ToggleDetails() {
+    const [currentId, setCurrentId] = useState(null)
+    
     return (
         <div>
         <ul>
             {TOGGLE_DATA.map(data => (
                 <div key={data.id}>
                     <li >{data.item}</li>
-                    <button>Show details</button>
+                    <button onClick={() => setCurrentId(data.id)}>Show details</button>
+                    <p></p>
                 </div>
             ))}
         </ul>
