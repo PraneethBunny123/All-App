5cf780412b27adca9a4995abecff32cc12691b43
praneeth <gp.gunturu@gmail.com>
2025-04-22 17:40:34 -0500

    Checking select and  option tags

--- src/project-apps/Dropdown/Dropdown.jsx
+++ src/project-apps/Dropdown/Dropdown.jsx
@@ -3,7 +3,10 @@ import COUNTRIES from "./countries"
 export default function Dropdown() {
     return (
         <div>
-            Hello
+            <select>
+                <option value="option 1">opt 1</option>   
+                <option value="option 2">opt 2</option>   
+            </select>
         </div>
     )
 }
