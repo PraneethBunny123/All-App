0113b8c9ad06119e536fcac53ecb01ecd0f7b48c
praneeth <gp.gunturu@gmail.com>
2025-04-23 17:25:36 -0500

    updating state to selected country when clicked

--- src/project-apps/Dropdown/CustomDropdown.jsx
+++ src/project-apps/Dropdown/CustomDropdown.jsx
@@ -2,24 +2,34 @@ import { useState } from "react"
 import COUNTRIES from "./countries"
 
 export default function CustomDropdown() {
-    const [selectedValue, setSelectedValue] = useState('')
+    const [selectedValue, setSelectedValue] = useState('select country')
     const [isOpen, setIsOpen] = useState(false)
 
-    function handleClick() {
+    function handleDropdown() {
         if(isOpen)
             setIsOpen(false)
         else
             setIsOpen(true)
     }
 
+    function handleSelectValue(country) {
+        setSelectedValue(country)
+        setIsOpen(false)
+    }
+
+    console.log(selectedValue)
+
     return (
         <div>
-            <button onClick={handleClick}>select country</button>
-            {isOpen && <ul>
-                {COUNTRIES.map(country => (
-                    <li key={country}>{country}</li>
-                ))}
-            </ul>
+            <button onClick={handleDropdown}>--{selectedValue}--</button>
+            {isOpen && 
+                <ul>
+                    {COUNTRIES.map(country => (
+                        <li key={country}>
+                            <button onClick={() => handleSelectValue(country)}>{country}</button>
+                        </li>
+                    ))}
+                </ul>
             }
         </div>
     )
