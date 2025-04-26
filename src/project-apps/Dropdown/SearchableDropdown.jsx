366bed3ec365e24b41dcd27116665e510b563ee3
praneeth <gp.gunturu@gmail.com>
2025-04-26 12:19:28 -0500

    Added ref to check if click is inside the input or outside. using that ref in the useEffect function to check for click via handeClickOutside function. which reads that ref and updates the state

--- src/project-apps/Dropdown/SearchableDropdown.jsx
+++ src/project-apps/Dropdown/SearchableDropdown.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import COUNTRIES from "./countries"
 
 export default function SearchableDropdown() {
     const [search, setSearch] = useState('')
     const [selectedCountry, setSelectedCountry] = useState([])
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+    const dropdownRef = useRef(null)
 
     function handleSearch(e) {
         e.preventDefault()
@@ -14,23 +17,39 @@ export default function SearchableDropdown() {
         setSelectedCountry(prevState => [...prevState, country])
         setSearch('')
     }
-    console.log(selectedCountry)
+    
+    function handleClickOutside(e) {
+        if(dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+            setIsDropdownOpen(false)
+            setSearch('')
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside)
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside)
+        }
+    }, [])
 
     const filteredCountries = COUNTRIES.filter(country => country.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <div>
             <label>Searchable Dropdown</label>
-            <input value={search} placeholder="search" type="text" onChange={handleSearch}/>
-            {
-            <ul>
-                {filteredCountries.map(country => (
-                    <li key={country}>
-                        <button disabled={selectedCountry.includes(country)} onClick={() => handleSelectCountry(country)}>{country}</button>
-                    </li>
-                ))}
-            </ul>
-            }
+            <div ref={dropdownRef}>
+                <input onFocus={() => setIsDropdownOpen(true)} value={search} placeholder="search" type="text" onChange={handleSearch}/>
+                {isDropdownOpen &&
+                <ul>
+                    {filteredCountries.map(country => (
+                        <li key={country}>
+                            <button disabled={selectedCountry.includes(country)} onClick={() => handleSelectCountry(country)}>{country}</button>
+                        </li>
+                    ))}
+                </ul>
+                }
+            </div>
             <div>
                 <label>Selected Countries</label>
                 <ul>
