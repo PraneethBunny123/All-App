f095d2ede4c5b3ab2d0f26a74ae742e4de4f73ba
praneeth <gp.gunturu@gmail.com>
2025-04-28 20:28:48 -0500

    setup state to handle the input fields values and storing them in the state

--- src/project-apps/Form/SignupForm.jsx
+++ src/project-apps/Form/SignupForm.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
+
 export default function SignupForm() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <div>
             <form>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
+                <input value={name} type="text" onChange={(e) => setName(e.target.value)}/>
+                <input value={email} type="text" onChange={(e) => setEmail(e.target.value)}/>
+                <input value={password} type="text" onChange={(e) => setPassword(e.target.value)}/>
                 <button>Submit</button>
             </form>
         </div>
