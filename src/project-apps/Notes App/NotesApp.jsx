6451bad146d9d176999afba7dba25ba404e5fe8c
praneeth <gp.gunturu@gmail.com>
2025-05-01 17:48:04 -0500

    adding notes to the array and rendering them and clearing the input after every field

--- src/project-apps/Notes App/NotesApp.jsx
+++ src/project-apps/Notes App/NotesApp.jsx
@@ -1,13 +1,32 @@
+import { useRef, useState } from "react"
 import groceries from "./MOCK_DATA"
 
 export default function NotesApp() {
+    const [notes, setNotes] = useState(groceries)
+    const [textTyped, setTextTyped] = useState('')
+
+    function handleOnChange(e) {
+        setTextTyped(e.target.value)
+    }
+
+    function handleAddNotes() {
+        setNotes(prevNotes => [...prevNotes, {
+            "id": prevNotes.length + 1,
+            "Notes": textTyped
+        }])
+        setTextTyped('')
+        
+    }
+
+
+
     return (
         <div>
-            <input type="text" placeholder="Add Note..."/>
-            <button>Add Note</button>
+            <input value={textTyped} onChange={handleOnChange} type="text" placeholder="Add Note..."/>
+            <button onClick={handleAddNotes}>Add Note</button>
 
             <ul>
-                {groceries.map(obj => (
+                {notes.map(obj => (
                     <li key={obj.id}>{obj.Notes}</li>
                 ))}
             </ul>
