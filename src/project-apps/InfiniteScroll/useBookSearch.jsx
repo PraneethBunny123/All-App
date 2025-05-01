5281d0353add640dc605cf7384eb222d8a3811e6
praneeth <gp.gunturu@gmail.com>
2025-04-30 19:02:57 -0500

    This request gives a response. as of now logging the res data

--- src/project-apps/InfiniteScroll/useBookSearch.jsx
+++ src/project-apps/InfiniteScroll/useBookSearch.jsx
@@ -6,7 +6,10 @@ export default function useBookSearch(query, pageNumber) {
     useEffect(() => {
         axios({
             method: 'GET',
-            url: 'https://openlibrary.org/search.json'
+            url: 'https://openlibrary.org/search.json',
+            params: {q: query, page: pageNumber}
+        }).then(res => {
+            console.log(res.data)
         })
     }, [query, pageNumber])
 
