65db522265127c4b24cff3aba42690a94fcb410b
praneeth <gp.gunturu@gmail.com>
2025-05-02 16:59:58 -0500

    Not adding notes if search bar is empty

--- src/project-apps/Notes App/NotesApp.jsx
+++ src/project-apps/Notes App/NotesApp.jsx
@@ -2,25 +2,30 @@ import { useState } from "react"
 import groceries from "./MOCK_DATA"
 
 export default function NotesApp() {
-    const [notes, setNotes] = useState(groceries)
+    const [notes, setNotes] = useState(() => localStorage.getItem('notes') || groceries)
     const [textTyped, setTextTyped] = useState('')
+
+    let filteredNotes = []
 
     function handleOnChange(e) {
         setTextTyped(e.target.value)
     }
 
     function handleAddNotes() {
-        setNotes(prevNotes => [...prevNotes, {
-            "id": prevNotes.length + 1,
-            "Notes": textTyped
-        }])
-        setTextTyped('')
+        if(textTyped.trim() !== '') {
+            setNotes(prevNotes => [...prevNotes, {
+                "id": prevNotes.length + 1,
+                "Notes": textTyped
+            }])
+            setTextTyped('')
+        }
     }
 
     function handleRemoveNote(id) {
-        const filteredNotes = notes.filter(note => note.id !== id)
+        filteredNotes = notes.filter(note => note.id !== id)
         setNotes(filteredNotes)
     }
+
 
 
     return (
