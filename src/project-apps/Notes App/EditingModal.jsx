f47895c823500344c8f183a111255f3878142d46
praneeth <gp.gunturu@gmail.com>
2025-05-04 14:11:54 -0500

    Accepting props and showing modal only when isOpen as true

--- src/project-apps/Notes App/EditingModal.jsx
+++ src/project-apps/Notes App/EditingModal.jsx
@@ -1,15 +1,17 @@
 import { useRef } from "react"
 import { createPortal } from "react-dom"
 
-export default function EditingModal() {
+export default function EditingModal({isOpen, onClose, onSave, initialValue}) {
     const dialogRef = useRef()
 
-    dialogRef.current.showModal()
+    if(isOpen)
+        dialogRef.current.showModal()
 
     return createPortal(
         <dialog ref={dialogRef}>
             <input type="text"/>
             <button>Save</button>
+            <button>Cancel</button>
         </dialog>,
         document.getElementById('modal')
     )
