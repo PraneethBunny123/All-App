aef7e306821bfb607d0c5aa15b3d7d5f6bef2473
praneeth <gp.gunturu@gmail.com>
2025-05-06 15:28:06 -0500

    Rendering tags seperately in the note card

--- src/project-apps/Notes App/NotesListRender.jsx
+++ src/project-apps/Notes App/NotesListRender.jsx
@@ -16,6 +16,13 @@ export default function NotesListRender({notes, setNotes, setEditingNote, setIsM
                 return (
                     <li key={obj.id} className="bg-white p-4 rounded-lg shadow border flex flex-col justify-between">
                         <div className="mb-2 text-gray-800 font-medium">{obj.Notes}</div>
+                        <div>
+                            {obj.tags.map(tag => (
+                                <span key={tag} className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
                         <span>{obj.timeStamp}</span>
                         <button onClick={() => handleEditButton(obj)}>✏️Edit</button>
                         <button onClick={() => handleRemoveNote(obj.id)}>❌</button>
