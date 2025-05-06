37e8399bbb289cc2d3f76d205c2807148feb7f6c
praneeth <gp.gunturu@gmail.com>
2025-05-05 21:57:14 -0500

    adding selected tags to the state

--- src/project-apps/Notes App/TagsDropdown.jsx
+++ src/project-apps/Notes App/TagsDropdown.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
 import { tagOptions } from "./MOCK_DATA"
 
 export default function TagsDropdown() {
+    const [selectedTags, setSelectedTags] = useState([])
+
+    function handleSelectTags(e) {
+        const tagsArray = Array.from(e.target.selectedOptions).map(tag => tag.value)
+
+        setSelectedTags(prevTags => 
+            [...new Set([...prevTags, ...tagsArray])]
+        )
+    }
+
+    console.log(selectedTags)
+
     return (
         <div>
             <select
                 multiple
+                onChange={handleSelectTags}
             >
                 {tagOptions.map(tag => (
                     <option key={tag} value={tag}>{tag}</option>
