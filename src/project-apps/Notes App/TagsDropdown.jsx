5b642bf67dc95bc34ea420353b62138c69cda332
praneeth <gp.gunturu@gmail.com>
2025-05-06 19:27:42 -0500

    replacing the custom select with the button for better UX

--- src/project-apps/Notes App/TagsDropdown.jsx
+++ src/project-apps/Notes App/TagsDropdown.jsx
@@ -12,16 +12,10 @@ export default function TagsDropdown({setSelectedTags}) {
 
     return (
         <div>
-        <label className="block mb-1 font-medium text-gray-700">Select Tags:</label>
-            <select
-                multiple
-                onChange={handleSelectTags}
-                className="w-full h-32 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            >
-                {tagOptions.map(tag => (
-                    <option key={tag} value={tag}>{tag}</option>
-                ))}
-            </select>
+            <label className="block mb-1 font-medium text-gray-700">Select Tags:</label>
+            <button>
+                Choose Tags
+            </button>
         </div>
     )
 }
