464b98b301ea9541722af33776447d66774cf7cb
praneeth <gp.gunturu@gmail.com>
2025-05-08 18:23:33 -0500

    Changes made in styling in SearchNoes

--- src/project-apps/Notes App/SearchNotes.jsx
+++ src/project-apps/Notes App/SearchNotes.jsx
@@ -11,7 +11,7 @@ export default function SearchNotes({searchNote, setSearchNote}) {
                 value={searchNote} 
                 onChange={handleSearchNote}
                 placeholder="🔍Search notes..."
-                className="w-full px-5 py-3 border border-gray-300 rounded-xl shadow focus:outline-none focus:ring-2 focus:ring-blue-400 bg-white"
+                className="w-full pl-10 pr-5 py-3 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 bg-white"
             />
         </div>
     )
