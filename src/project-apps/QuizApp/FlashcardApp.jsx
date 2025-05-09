33844636c56e3faa0066013901bceccbd1c955d9
praneeth <gp.gunturu@gmail.com>
2025-05-09 17:15:11 -0500

    Added the state to conditionally render the question or answer

--- src/project-apps/QuizApp/FlashcardApp.jsx
+++ src/project-apps/QuizApp/FlashcardApp.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { QUESTIONS } from "./QUESTIONS.JS";
 
 export default function FlashcardApp() {
+    const [isQuestionVisible, setIsQuestionVisible] = useState(true)
+
+    function handleCardClick() {
+
+    }
+
     return (
         <div>
             <ul>
                 {QUESTIONS.map(question => (
-                    <button key={question.id}>
-                        {question.question}
+                    <button key={question.id} onClick={handleCardClick}>
+                        {isQuestionVisible ? question.question : question.answer}
                     </button>
                 ))}
             </ul>
