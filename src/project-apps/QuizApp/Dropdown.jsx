f0547a6160d0b8cf6d5c0b5b9ad2b629f5da05ad
praneeth <gp.gunturu@gmail.com>
2025-05-09 20:13:30 -0500

    Setup state to render the categories

--- src/project-apps/QuizApp/Dropdown.jsx
+++ src/project-apps/QuizApp/Dropdown.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { QUESTIONS } from "./QUESTIONS.JS";
 
 export default function Dropdown() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
     return (
         <div>
             <button>Select Category</button>
-            {QUESTIONS.map(question => 
-                <button>{question.category}</button>
-            )}
+            {isDropdownOpen &&
+                QUESTIONS.map(question => 
+                    <button key={question.id}>{question.category}</button>
+                )
+            }
         </div>
     )
 }
