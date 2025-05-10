b46f3f200a1ef393fb9dd0315ea096f82b88b30c
praneeth <gp.gunturu@gmail.com>
2025-05-10 13:30:30 -0500

    Added dropdown styles to the dropdown button

--- src/project-apps/QuizApp/Dropdown.jsx
+++ src/project-apps/QuizApp/Dropdown.jsx
@@ -10,7 +10,15 @@ export default function Dropdown({allCategories, selectedCategory, setSelectedCa
 
     return (
         <div className="relative inline-block text-left">
-            <button onClick={() => setIsDropdownOpen(prevState => !prevState)}>Select Category</button>
+            <button 
+                onClick={() => setIsDropdownOpen(prevState => !prevState)}
+                className="inline-flex justify-center items-center px-4 py-2 text-sm font-medium bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none"
+            >
+                Select Category
+                <svg className="ml-2 h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.584l3.71-4.354a.75.75 0 011.14.976l-4.25 5a.75.75 0 01-1.14 0l-4.25-5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+                </svg>
+            </button>
             {isDropdownOpen &&
                 <ul>
                     {allCategories.map(category => 
