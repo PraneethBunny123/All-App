62c6cafacce35bddf807bd4674f747e766c55b29
praneeth <gp.gunturu@gmail.com>
2025-05-21 16:11:28 -0500

    setup state and onchange to the input to store the entered value on every key stroke

--- src/project-apps/Debounce/Debounce.jsx
+++ src/project-apps/Debounce/Debounce.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+
 export default function Debounce() {
+    const [input, setInput] = useState('')
+
+    function handleOnchange(e) {
+        setInput(e.target.value)
+    }
+
     return (
         <div>
-            <input type="text" placeholder="enter text"/>
+            <input 
+                type="text" 
+                placeholder="enter text"
+                value={input}
+                onChange={handleOnchange}    
+            />
             
             <div>
                 <p>Default: {}</p>
