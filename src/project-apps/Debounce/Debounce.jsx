63adf65a3cfee98d4d2a00de582c277d16dda3cc
praneeth <gp.gunturu@gmail.com>
2025-05-21 15:55:47 -0500

    Created the basic outline to display the text typed from the inout field

--- src/project-apps/Debounce/Debounce.jsx
+++ src/project-apps/Debounce/Debounce.jsx
@@ -1,7 +1,12 @@
 export default function Debounce() {
     return (
         <div>
-            Hello
+            <input type="text" placeholder="enter text"/>
+            
+            <div>
+                <p>Default: {}</p>
+                <p>Debounce: {}</p>
+            </div>
         </div>
     )
 }
