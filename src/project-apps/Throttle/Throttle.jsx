9eb80c78f5a95a5a659326fe860e78c293834634
praneeth <gp.gunturu@gmail.com>
2025-05-22 00:37:15 -0500

    Created a outline of throttle function and setup a state for that. As of now nothing happens

--- src/project-apps/Throttle/Throttle.jsx
+++ src/project-apps/Throttle/Throttle.jsx
@@ -2,10 +2,20 @@ import { useState } from "react"
 
 export default function Throttle() {
     const [defaultInput, setDefaultInput] = useState('')
+    const [throttleInput, setThrottleInput] = useState('')
 
     function handleOnchange(e) {
         setDefaultInput(e.target.value)
+        
     }
+
+    function handleThrottle(cb, delay=1000) {
+        let shouldWait = false
+
+        cb()
+        shouldWait = true
+    }
+
 
     return (
         <div>
@@ -19,3 +29,4 @@ export default function Throttle() {
         </div>
     )
 }
+
