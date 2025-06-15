6e474ac0ace2d0d6aeef6703908187490d81459e
praneeth <gp.gunturu@gmail.com>
2025-06-15 13:03:17 -0500

    Created a form outline for the demo form

--- src/project-apps/Form/ContactForm.jsx
+++ src/project-apps/Form/ContactForm.jsx
@@ -1,5 +1,21 @@
 export default function ContactForm() {
     return (
-        <div>ContactForm</div>
+        <section>
+            <form>
+                <h2>Contact Form</h2>
+                <div>
+                    <label>Full Name</label>
+                    <input type="text" placeholder="enter full name" required />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="text" placeholder="enter email" required />
+                </div>
+                <div>
+                    <label>Message</label>
+                    <input type="text" placeholder="enter your message" required />
+                </div>
+            </form>
+        </section>
     )
 }
