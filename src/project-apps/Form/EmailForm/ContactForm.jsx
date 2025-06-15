60d8602539ac391417b577dd920610cdf4c7ff27
praneeth <gp.gunturu@gmail.com>
2025-06-15 13:44:31 -0500

    Successfully added the mail notification when form is submitted using web3Forms

--- src/project-apps/Form/EmailForm/ContactForm.jsx
+++ src/project-apps/Form/EmailForm/ContactForm.jsx
@@ -1,21 +1,47 @@
 import './ContactForm.css'
 
 export default function ContactForm() {
+
+    async function onSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+
+        formData.append("access_key", "94f8b0a9-fd1f-48df-b31f-91e3335312cb");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        })
+        
+        const resData = await response.json()
+
+        if (resData.success) {
+            console.log("Success", resData);
+        }
+    }
+
     return (
         <section className='contact'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <h2>Contact Form</h2>
                 <div className='input-box'>
                     <label>Full Name</label>
-                    <input type="text" placeholder="enter full name" className='field' required />
+                    <input type="text" placeholder="enter full name" className='field' name='name' required />
                 </div>
                 <div className='input-box'>
                     <label>Email</label>
-                    <input type="text" placeholder="enter email" className='field' required />
+                    <input type="text" placeholder="enter email" className='field' name='email' required />
                 </div>
                 <div className='input-box'>
                     <label>Message</label>
-                    <textarea type="text" placeholder="enter your message" className='field mess' required />
+                    <textarea type="text" placeholder="enter your message" className='field mess' name='message' required />
                 </div>
                 <button type="submit">Send message</button>
             </form>
